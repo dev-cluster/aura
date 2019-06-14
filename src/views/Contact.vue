6c3086f88ff75b2701451e1ed5e63e51f9d07eb2
shreykeny <shreykeny@gmail.com>
2019-06-14 19:00:49 +0530

    some more changes to Contact section

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,7 +6,7 @@
           
           <p class="google-font mb-0 mt-3" style="font-size:200%;color:#616161">Contact {{ChapterDetails.ChapterName}}</p>
           <!-- <v-divider></v-divider> -->
-          <p class="google-font mt-3" style="font-size:110%">Our events are open to newbies, developers, managers, and organizations who are interested in technology and useful tools</p>
+          <p class="google-font mt-3" style="font-size:110%">Our events are open to newbies, students, developers, managers, and organizations who are interested in upcoming and relevant technologies and tools enabling developers.</p>
           
           <p class="google-font mb-0" style="font-size:130%">Social Media:</p>
           <p class="google-font mt-0 ml-0" style="font-size:130%" >
@@ -20,7 +20,7 @@
             </span>
           </p>
             
-            <p class="google-font mb-0" style="font-size:120%">Questions? Please contact {{ChapterDetails.ChapterEmail}}.</p>
+            <p class="google-font mb-0" style="font-size:120%">Questions? Please contact {{ChapterDetails.ChapterEmail}}</p>
         </v-flex>
         <v-flex xs12 md2 lg2 class="pa-2">
           <v-img
