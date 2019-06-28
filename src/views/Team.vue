7fa66cbcc19304d52fe8c93b9afb18d444d59483
Vipul Chodankar <vipul72c@gmail.com>
2019-06-28 22:16:26 +0530

    Updated Team and and chapterDetails

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -4,7 +4,7 @@
     <v-container fluid class=" mb-0" style="background-color:#303030">
       <v-layout wrap align-center justify-center row fill-height class="my-0">
         <v-flex xs12 md10 class="my-0">
-            <p class="google-font mb-0" style="font-size:200%;color:#0277BD">Our lineup of big thinkers and even bigger doers</p>
+            <p class="google-font mb-0" style="font-size:200%;color:#0277BD">Our band of big thinkers and go-getters</p>
             <p class="google-font mt-0 mb-0" style="font-size:110%">Here at Dev Cluster, we aren't just a team. We are a family. A family that brings people like you to learn and grow together.</p>
 
             <p class="google-font mt-3 mb-0" style="font-size:110%">Our goal is to organize space to connect the best industry experts with Indian audience to boost development of IT. And our Core Team is: </p>
