9d6bd3b8ad14adcff8f5ed797c5d572f83e18c70
Vipul Chodankar <vipul72c@gmail.com>
2019-06-28 21:09:19 +0530

    Updated Mrunal's twitter, Core Team github changed to email, changed Home intro

--- src/components/team/coreTeam.vue
+++ src/components/team/coreTeam.vue
@@ -25,6 +25,10 @@
                     <p class="mt-3 mb-0 google-font" style="font-size:130%">{{item.name}}</p>
                     <p class="mt-1 mb-0 google-font">{{item.designation}}</p>
 
+                    <v-btn class="mt-0 mx-0" icon v-if="(item.email).length>0" :href="item.email" target="_blank">
+                        <v-icon small style="color:#fff">email</v-icon>
+                    </v-btn>
+                    
                     <v-btn class="mt-0 mx-0" icon v-if="(item.twitter).length>0" :href="item.twitter" target="_blank">
                         <v-icon small style="color:#1da1f2">fab fa-twitter</v-icon>
                     </v-btn>
@@ -33,11 +37,10 @@
                         <v-icon small style="color:#0077b5">fab fa-linkedin-in</v-icon>
                     </v-btn>
 
-                    <v-btn class="mt-0 mx-0" icon v-if="(item.github).length>0" :href="item.github" target="_blank">
-                        <v-icon small style="color:#fff">fab fa-github</v-icon>
-                    </v-btn>
+                    
+
                     <v-btn class="mt-0 mx-0" icon v-if="(item.website).length>0" :href="item.website" target="_blank">
-                        <v-icon style="color:#333">link</v-icon>
+                        <v-icon style="color:#fff">link</v-icon>
                     </v-btn>
                 </div>
             </v-flex>
