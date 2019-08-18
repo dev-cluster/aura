b66e8d70bc9444e09b461d10873ff71f03f93691
Vipul Chodankar <vipul72c@gmail.com>
2019-08-18 18:06:59 +0530

    New Build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.9daa9ac9b7d1a5270085b24d0cac13c9.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.46517ec0701c9e10457aabb3a99f65c0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
